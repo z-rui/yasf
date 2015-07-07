dc972c8cfc8d651a1a5ee4b3903ef13c74d014ff
张睿 <zrui16@hotmail.com>
2015-07-08 00:05:13 +0800

    Move Scintilla config to LED file.

--- src/cb.c
+++ src/cb.c
@@ -12,20 +12,6 @@ int cb_main_init(Ihandle *ih)
 	Ihandle *cmdline;
 
 	// initialization work
-	cmdline = IupGetHandle("ctl_cmdline");
-	fprintf(stderr, IupGetAttribute(cmdline, "KEYWORDSETS"));
-	IupSetAttribute(cmdline, "KEYWORDS0",
-"abort action add after all alter analyze and as asc attach autoincrement before begin between by cascade case cast check collate column commit conflict constraint create cross current_date current_time current_timestamp database default deferrable deferred delete desc detach distinct drop each else end escape except exclusive exists explain fail for foreign from full glob group having if ignore immediate in index indexed initially inner insert instead intersect into is isnull join key left like limit match natural no not notnull null of offset on or order outer plan pragma primary query raise recursive references regexp reindex release rename replace restrict right rollback row savepoint select set table temp temporary then to transaction trigger union unique update using vacuum values view virtual when where with without");
-	IupSetRGBId(cmdline, "STYLEFGCOLOR", 1, 0, 128, 0);    // 1-C comment�
-	IupSetRGBId(cmdline, "STYLEFGCOLOR", 2, 0, 128, 0);    // 2-C++ comment line�
-	IupSetRGBId(cmdline, "STYLEFGCOLOR", 4, 255, 0, 255);    // 4-Number�
-	IupSetRGBId(cmdline, "STYLEFGCOLOR", 5, 0, 0, 255);    // 5-Keyword�
-	IupSetAttributeId(cmdline, "STYLEBOLD", 5, "YES");
-	IupSetRGBId(cmdline, "STYLEFGCOLOR", 6, 160, 20, 20);  // 6-String�
-	IupSetRGBId(cmdline, "STYLEFGCOLOR", 7, 128, 0, 0);    // 7-Character�
-	IupSetRGBId(cmdline, "STYLEFGCOLOR", 9, 0, 0, 255);    // 9-Preprocessor block�
-	IupSetRGBId(cmdline, "STYLEFGCOLOR", 10, 0, 0, 0); // 10-Operator�
-
 	cb_file_new(ih);
 
 	return IUP_DEFAULT;
